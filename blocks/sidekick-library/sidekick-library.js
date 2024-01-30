8f4640eff7c4ebb997e2b5cb6bc84ef4c5b7bf39
vmadhiyan <vikram.madhiyan@accenture.com>
2024-01-30 17:12:40 +0100

    Revert "new blocks"
    This reverts commit 5b9482c8ae5c6ca1a70dd73a1f58772e08765ad8.

--- blocks/sidekick-library/sidekick-library.js
+++
@@ -1,6 +0,0 @@
-import '../../tools/sidekick/library/index.js';
-
-export default function decorate(block) {
-  const library = document.createElement('sidekick-library');
-  block.replaceChildren(library);
-}
